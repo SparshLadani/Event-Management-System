a90b7afc1d0f3016d081e1fc4367278220747746
Sparsh Amitbhai Ladani <sp835101@dal.ca>
2024-05-06 11:11:47 -0300

    Pushing the code for event management system

--- Bootstrap/serverlogin.php
+++
@@ -1,6 +0,0 @@
-<?php // server login.php 
-  	$db_hostname = 'localhost';  
- 	$db_username = 'root';  //your username
-  	$db_password = 'root'; //your password
-    $db_database = 'whats_happening'; //database name 
-?>
